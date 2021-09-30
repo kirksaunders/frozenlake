0e05db744012f991d88fe53e2f60f5a4f139f186
kirksaunders <kirksaunders12@gmail.com>
2021-09-30 04:21:01 -0400

    Added monte carlo and q learning

--- example_interaction.py
+++ example_interaction.py
@@ -1,4 +1,4 @@
-#Requirements: 
+# Requirements: 
 # python3.x
 # gym (pip install gym)
 import random
@@ -7,8 +7,10 @@ import numpy as np
 
 random.seed(0)
 
-#This 'special' Frozen Lake allows access to transition dynamics
-from FrozenLakeEnv import FrozenLakeEnv
+#from gym.envs.toy_text import FrozenLakeEnv
+from frozen_lake import FrozenLakeEnv
+
+MAX_ITERATIONS = 1000
 
 def value_iteration(env, theta, gamma):
     # Initialize state value function to all 0's
@@ -92,67 +94,95 @@ def policy_iteration(env, theta, gamma):
 
     return (v, pi)
 
+def evaluate(env, pi, num_eps=100):
+    total_reward = 0
+    for i in range(num_eps):
+        state, done = env.reset(), False
+        it = 0
+        while not done and it < MAX_ITERATIONS:
+            action = pi[state]
+            state, reward, done, _ = env.step(action)
+            total_reward += reward
+            it += 1
+    print("Total reward is {} out of {} episodes ({}%)".format(total_reward, num_eps, (total_reward / num_eps)*100))
+
+def monte_carlo(env, gamma):
+    q = np.random.rand(env.nS, env.nA)
+    c = np.zeros((env.nS, env.nA))
+    pi = np.zeros(env.nS).astype(int)
+
+    iterations = 0
+    while True:
+        # Generate episode with equal probility stochastic policy
+        s = []
+        a = []
+        r = []
+        state, done = env.reset(), False
+        it = 0
+        while not done and it < MAX_ITERATIONS:
+            s.append(state)
+            action = env.action_space.sample()
+            state, reward, done, _ = env.step(action)
+            a.append(action)
+            r.append(reward)
+            it += 1
+
+        g = 0
+        w = 1
+        for t in reversed(range(len(s))):
+            g = gamma*g + r[t]
+            c[s[t], a[t]] += w
+            q[s[t], a[t]] += w/c[s[t], a[t]] * (g - q[s[t], a[t]])
+            pi[s[t]] = np.argmax(q[s[t], :])
+
+            if a[t] != pi[s[t]]:
+                break
+            w *= env.nA
+
+        iterations += 1
+        if iterations % 10000 == 0:
+            print("{} iterations of monte carlo control completed, evaluating policy".format(iterations))
+            evaluate(env, pi)
+            
+def q_learning(env, gamma, alpha, epsilon):
+    q = np.zeros((env.nS, env.nA))
+
+    iterations = 0
+    while True:
+        it = 0
+        state, done = env.reset(), False
+        while not done and it < MAX_ITERATIONS:
+            if random.random() > epsilon:
+                action = np.argmax(q[state, :])
+            else:
+                action = np.random.choice(np.arange(0, env.nA))
+            
+            new_state, reward, done, _ = env.step(action)
+            q[state, action] += alpha * (reward + gamma*np.max(q[new_state, :]) - q[state, action])
+            state = new_state
+
+        iterations += 1
+        if iterations % 10000 == 0:
+            print("{} iterations of q-learning completed, evaluating policy".format(iterations))
+            pi = np.zeros(env.nS).astype(int)
+            for i in range(env.nS):
+                pi[i] = np.argmax(q[i, :])
+            evaluate(env, pi)
 
 def main():
     # Stochastic
     env = FrozenLakeEnv(map_name="4x4", is_slippery=True)
     env.seed(0) #set env seed
 
-    v, pi = value_iteration(env, 1e-6, 1)
-    #v, pi = policy_iteration(env, 1e-6, 1)
+    v, pi = value_iteration(env, 1e-6, 0.999)
+    #v, pi = policy_iteration(env, 1e-6, 0.999)
 
-    np.set_printoptions(linewidth=1000)
-    print("state value function:\n", np.reshape(v, (4,4)))
-    print("optimal policy:\n", np.reshape(pi, (4,4)))
+    evaluate(env, pi)
 
-    # Interact with environment following optimal policy
-    total_reward = 0
-    for episode in range(10000):
-        state = env.reset()
-        for t in range(10000):
-            #env.render()
-            #sleep(1)
-            action = pi[state]
-            state, reward, done, info = env.step(action)
-            total_reward += reward
-            if done:
-                #print("Episode {} finished after {} timesteps with reward {}".format(episode, t+1, reward))
-                break
-    print("Total reward: {}".format(total_reward))
+    #monte_carlo(env, 0.999)
+    q_learning(env, 0.999, 0.1, 0.2)
+
     env.close()
-
-    # State Layout
-    # 0  1  2  3
-    # 4  5  6  7
-    # 8  9  10 11
-    # 12 13 14 15
-
-    #current_state = 14  # State from S_n=16 State space
-    #action = 2  # Left action from A_n=4 Action space
-
-    #print(env.P[current_state][action])
-
-    # [(0.3333333333333333, 14, 0.0, False), #0.3333333333333333 chance action 2 in state 14 moves to state 14, receives reward 0, and does not terminate the episode
-    # (0.3333333333333333, 15, 1.0, True), #0.3333333333333333 chance action 2 in state 14 moves to state 15, receives reward 1, and terminates the episode
-    # (0.3333333333333333, 10, 0.0, False)] #0.3333333333333333 chance action 2 in state 14 moves to state 10, receives reward 0, and does not terminate the episode
-
-    #env.reset()
-    #env.render()
-
-    #For more information on FrozenLake see: https://github.com/openai/gym/wiki/FrozenLake-v0
-    #For more information on installation and interaction with openai gym see: http://gym.openai.com/docs/ 
-    #let's randomly interact with the environment for 20 episodes
-    #for i_episode in range(20):
-    #    observation = env.reset()
-    #    for t in range(100):
-    #        env.render()
-    #        sleep(1)
-    #        action = env.action_space.sample()
-    #        observation, reward, done, info = env.step(action)
-    #        if done:
-    #            print("Episode finished after {} timesteps".format(t+1))
-    #            break
-    #env.close()
 
 if __name__=='__main__':
     main()
